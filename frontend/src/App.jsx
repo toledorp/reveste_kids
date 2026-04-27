ebbe751c5829cb20c98c818d2206f35cb7f3cab9
toledorp <roge.pupo.toledo@gmail.com>
2026-04-27 13:45:34 -0300

    chore: remove arquivos antigos e simplifica frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,15 +6,14 @@ import {
   useNavigate,
   useLocation,
 } from "react-router-dom";
-import Navbar from "./components/Navbar";
+
 import Footer from "./components/Footer";
 import AuthPanel from "./components/AuthPanel";
-import Feed from "./pages/Feed";
 import FeedTikTok from "./pages/FeedTikTok";
 import MyCloset from "./pages/MyCloset";
 import AddClothing from "./pages/AddClothing";
 import Matches from "./pages/Matches";
-import ChatTest from "./pages/ChatTest";
+
 
 function LoginPage() {
   const navigate = useNavigate();
@@ -50,12 +49,10 @@ function AppContent() {
 
         <Route path="/feed" element={<FeedTikTok />} />
         <Route path="/feed-tiktok" element={<FeedTikTok />} />
-        <Route path="/feed-antigo" element={<Feed />} />
 
         <Route path="/closet" element={<MyCloset />} />
         <Route path="/add-clothing" element={<AddClothing />} />
         <Route path="/matches" element={<Matches />} />
-        <Route path="/chat-test" element={<ChatTest />} />
 
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
