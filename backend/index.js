47efa67cd5c0bd07dd12cc9e72e88bc09ffadda9
toledorp <roge.pupo.toledo@gmail.com>
2026-05-10 11:26:42 -0300

    Fix Render PORT env

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,15 @@ const server = http.createServer(app);
 const port = process.env.PORT || 4000;
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://reveste-kids-sepia.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
